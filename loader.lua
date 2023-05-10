14e7bc18c3bd0e4695f4240291e2606f35a51cc8
CeleryScripts <132805114+CeleryScripts@users.noreply.github.com>
2023-05-10 21:31:42 +0900

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,19 +1,84 @@
 -- created by the fbi
--- < Dont Change >
-loadstring(game:HttpGet('https://raw.githubusercontent.com/CeleryScripts/simple-execution-logs/main/FixLoadstring.lua'))()
-task.wait(0.2)
-local v1 = "";
-if (game:GetService("UserInputService").TouchEnabled and (game:GetService("UserInputService").KeyboardEnabled == false)) then
-	v1 = "Mobile User";
+-- << settings >> 
+local webhook = "" -- put your webhook here
+-- << Dont Change. >> --
+local namecall
+namecall = hookmetamethod(game, "__namecall", function(self, ...)
+    local method = getnamecallmethod()
+	local caller = checkcaller()
+    if method == "HttpGet" and caller then
+		warn("Running Loadstring!")
+          return httpget(...)
+	end
+    return namecall(self, ...)
+end)
+local device = ""
+if game:GetService("UserInputService").TouchEnabled and game:GetService("UserInputService").KeyboardEnabled == false then
+    device = "Mobile User"
 else
-	v1 = "PC or Console";
+	device = "PC or Console"
 end
-task.wait(0.2)
-local v2 = game:GetService("Players").LocalPlayer;
-local v3 = false;
-local v4 = identifyexecutor();
-local v5 = game.Players.LocalPlayer;
-local v6 = os.time();
-local v7 = os.date("!*t", v6);
-local v8 = {title="__**Execution Logs.**__",description="",type="rich",color=tonumber(16776960),thumbnail={url=("https://www.roblox.com/asset-thumbnail/image?assetId=" .. game.PlaceId .. "&width=768&height=432")},image={url=""},fields={{name="__Username__",value=v5.Name,inline=false},{name="__UserID__",value=v5.UserId,inline=false},{name="__GameID__",value=game.PlaceId,inline=false},{name="__JobID__",value=game.JobId,inline=false},{name="__Executor__",value=v4,inline=false},{name="__Device__",value=v1,inline=false}},footer={text="Execution logs",icon_url=""},timestamp=string.format("%d-%d-%dT%02d:%02d:%02dZ", v7.year, v7.month, v7.day, v7.hour, v7.min, v7.sec)};
-((syn and syn.request) or http_request or http.request)({Url=webhook,Method="POST",Headers={["Content-Type"]="application/json"},Body=game:GetService("HttpService"):JSONEncode({content=Content,embeds={v8}})});
+wait(1)
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local IdentifyExecutor = false
+local RetrieveExecutor = identifyexecutor()
+local plr = game.Players.LocalPlayer
+local OSTime = os.time()
+local Time = os.date('!*t', OSTime)
+local Embed = {
+			["title"] = "__**Execution Logs.**__",
+			["description"] = "",
+			["type"] = "rich",
+			["color"] = tonumber(0xffff00),
+	                ["thumbnail"] = {
+				["url"] = "https://www.roblox.com/asset-thumbnail/image?assetId="..game.PlaceId.."&width=768&height=432"
+			},
+			["image"] = {
+				["url"] = ""
+			},
+			["fields"] = {
+			    	{
+					["name"] = "__Username__",
+					["value"] = plr.Name,
+					["inline"] = false
+				},
+							    	{
+					["name"] = "__UserID__",
+					["value"] = plr.UserId,
+					["inline"] = false
+				},
+											    	{
+					["name"] = "__GameID__",
+					["value"] = game.PlaceId,
+					["inline"] = false
+				},
+															    	{
+					["name"] = "__JobID__",
+					["value"] = game.JobId,
+					["inline"] = false
+				},
+																			    	{
+					["name"] = "__Executor__",
+					["value"] = RetrieveExecutor,
+					["inline"] = false
+				},
+																							    	{
+					["name"] = "__Device__",
+					["value"] = device,
+					["inline"] = false
+				}
+			},
+			["footer"] = {
+			    ["text"] = "Execution logs",
+			    ["icon_url"] = ""
+			},
+			["timestamp"] = string.format('%d-%d-%dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec),
+};
+(syn and syn.request or http_request or http.request) {
+    Url = webhook;
+    Method = 'POST';
+    Headers = {
+        ['Content-Type'] = 'application/json';
+    };
+    Body = game:GetService'HttpService':JSONEncode({content = Content; embeds = {Embed}; });
+};
