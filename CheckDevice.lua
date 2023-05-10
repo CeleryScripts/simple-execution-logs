ccb86222c39c1a371a223976559261cc4bb9ff06
CeleryScripts <132805114+CeleryScripts@users.noreply.github.com>
2023-05-10 20:36:43 +0900

    Create CheckDevice.lua

---
+++ CheckDevice.lua
@@ -0,0 +1,6 @@
+local v1 = "";
+if (game:GetService("UserInputService").TouchEnabled and (game:GetService("UserInputService").KeyboardEnabled == false)) then
+	v1 = "Mobile User";
+else
+	v1 = "PC or Console";
+end
